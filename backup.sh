b1ed8384f0d5776a9ec9069e09ab3dcefd14f6f6
zouzj <root@zend-ThinkPad-R61.(none)>
2012-08-04 23:42:04 +0800

    build src tools

--- backup.sh
+++ backup.sh
@@ -2,4 +2,4 @@
 rm ./initramfs/* -rf
 rm ./deplibs/* -rf
 set -x
-tar --exclude 'build-from-source/*' -zcvf /mnt/hgfs/vmshare/builder.backup.`date -Imin|sed 's/:/-/g'`.tgz *
+tar --exclude 'build-from-source/*' --exclude 'source-pkgs' -zcvf /mnt/hgfs/vmshare/builder.backup.`date -Imin|sed 's/:/-/g'`.tgz *
