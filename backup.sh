55cc2a48905799cf353f0365b2963fa88310a195
steven.zou <zouzj.2009@gmail.com>
2012-10-22 20:37:55 -0700

    in-redhat6.004

--- backup.sh
+++ backup.sh
@@ -2,4 +2,5 @@
 rm ./initramfs/* -rf
 rm ./deplibs/* -rf
 set -x
-tar --exclude 'build-from-source/*' --exclude 'source-pkgs' -zcvf /mnt/hgfs/vmshare/builder.backup.`date -Imin|sed 's/:/-/g'`.tgz *
+tar --exclude='.git' --exclude 'build-from-source/*' --exclude 'source-pkgs' -zcvf /mnt/hgfs/vmshare/builder.backup.`date -Imin|sed 's/:/-/g'`.tgz *
+
